2261e23306b47db8e0a13861347064d7886fe9eb
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-24 04:04:46 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   # Add your GamesController code here
   def create
     game = Game.new()
-    render json.game
+    render json: game
   end
 
   def update
