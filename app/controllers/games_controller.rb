5c3f9543d5dc2fb8db3a29e56cd257131cb94eec
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-24 03:48:11 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,12 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
+    game = Game.new(game_params)
+    render json.game
+  end
 
+  private
+  def game_params
+    params.require(:game).permit(:state)
   end
 end
